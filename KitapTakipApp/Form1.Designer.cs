a168e29d7884a57db5cb24fbd53e00e81f56109e
Ardacaan7 <ardacangokce07@gmail.com>
2025-05-14 20:21:18 +0300

    KitapTakipApp
    Günlük kaç sayfa kitap okuduğumuzu gösteren uygulama

---
+++ KitapTakipApp/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace KitapTakipApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtKitapAdi = new System.Windows.Forms.TextBox();
+            this.numSayfa = new System.Windows.Forms.NumericUpDown();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.listBoxKitaplar = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblToplamSayfa = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timerHatirlat = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numSayfa)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtKitapAdi
+            // 
+            this.txtKitapAdi.Location = new System.Drawing.Point(175, 125);
+            this.txtKitapAdi.Name = "txtKitapAdi";
+            this.txtKitapAdi.Size = new System.Drawing.Size(99, 20);
+            this.txtKitapAdi.TabIndex = 0;
+            // 
+            // numSayfa
+            // 
+            this.numSayfa.Location = new System.Drawing.Point(175, 152);
+            this.numSayfa.Name = "numSayfa";
+            this.numSayfa.Size = new System.Drawing.Size(99, 20);
+            this.numSayfa.TabIndex = 1;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(105, 182);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // listBoxKitaplar
+            // 
+            this.listBoxKitaplar.FormattingEnabled = true;
+            this.listBoxKitaplar.Location = new System.Drawing.Point(105, 211);
+            this.listBoxKitaplar.Name = "listBoxKitaplar";
+            this.listBoxKitaplar.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBoxKitaplar.Size = new System.Drawing.Size(169, 43);
+            this.listBoxKitaplar.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(102, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Sayfa Sayısı:";
+            // 
+            // lblToplamSayfa
+            // 
+            this.lblToplamSayfa.AutoSize = true;
+            this.lblToplamSayfa.Location = new System.Drawing.Point(101, 275);
+            this.lblToplamSayfa.Name = "lblToplamSayfa";
+            this.lblToplamSayfa.Size = new System.Drawing.Size(35, 13);
+            this.lblToplamSayfa.TabIndex = 5;
+            this.lblToplamSayfa.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(102, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Kitap Adı :";
+            // 
+            // timerHatirlat
+            // 
+            this.timerHatirlat.Enabled = true;
+            this.timerHatirlat.Interval = 6000;
+            this.timerHatirlat.Tick += new System.EventHandler(this.timerHatirlat_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblToplamSayfa);
+            this.Controls.Add(this.listBoxKitaplar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.numSayfa);
+            this.Controls.Add(this.txtKitapAdi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numSayfa)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKitapAdi;
+        private System.Windows.Forms.NumericUpDown numSayfa;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ListBox listBoxKitaplar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblToplamSayfa;
+        public System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timerHatirlat;
+    }
+}
+
---
+++ KitapTakipApp/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KitapTakipApp
+{
+    public partial class Form1: Form
+    {
+        int toplamSayfa = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            lblToplamSayfa.Text = "Toplam Okunan Sayfa: 0";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timerHatirlat.Interval = 60000;
+            timerHatirlat.Tick += timerHatirlat_Tick;
+            timerHatirlat.Start();
+        }
+
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            string kitapAdi = txtKitapAdi.Text;
+            int sayfaSayisi = (int)numSayfa.Value;
+
+            if (string.IsNullOrWhiteSpace(kitapAdi) || sayfaSayisi == 0)
+            {
+                MessageBox.Show("Lütfen kitap adı ve sayfa sayısını girin.");
+                return;
+            }
+
+            
+            listBoxKitaplar.Items.Add($"{kitapAdi} - {sayfaSayisi} sayfa");
+
+            
+            toplamSayfa += sayfaSayisi;
+            lblToplamSayfa.Text = $"Toplam Okunan Sayfa: {toplamSayfa}";
+
+            
+            txtKitapAdi.Clear();
+            numSayfa.Value = 0;
+        }
+
+        private void timerHatirlat_Tick(object sender, EventArgs e)
+        {
+            if (DateTime.Now.Hour == 19 && DateTime.Now.Minute == 0)
+            {
+                MessageBox.Show("Gün bitmeden birkaç sayfa kitap oku!", "Kitap Hatırlatıcısı");
+                timerHatirlat.Stop();
+            }
+        }
+    }
+}
